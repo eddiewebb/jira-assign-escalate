eb3ae7d75dc7f9ece9f44f1019ce35d29a67156e
EddieWebb <Ollitech@gmail.com>
2014-08-17 17:53:13 -0400

    release ready

--- src/test/java/it/com/edwardawebb/jira/assignescalate/rest/AssignmentServiceRestTest.java
+++ src/test/java/it/com/edwardawebb/jira/assignescalate/rest/AssignmentServiceRestTest.java
@@ -33,7 +33,7 @@ public class AssignmentServiceRestTest extends AbstractEndpointTest {
     public void testANewSupportTeamCanBeCreated(){
         StringBuilder formData = new StringBuilder();
         formData.append("prj_id=10001");
-        formData.append("&name=Level One Team - Awesome");
+        formData.append("&name=Level One Team");
         formData.append("&role=Developers");
         ClientResponse response = client.resource(resourceUrlTeam + "/team").contentType(MediaType.APPLICATION_FORM_URLENCODED).accept(MediaType.APPLICATION_JSON).post(formData.toString());
         assertEquals("Could not create team",200,response.getStatusCode());
