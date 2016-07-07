fb3e245570d4366a05476bae474dd4e522bdcb6c
EddieWebb <Ollitech@gmail.com>
2016-07-07 08:19:00 -0400

    fixed ket setup for pipelines

--- pipelines/setupSsh.sh
+++ pipelines/setupSsh.sh
@@ -8,7 +8,7 @@ cat /etc/ssh/ssh_config
 echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 cat /etc/ssh/ssh_config
 mkdir -p ~/.ssh
-echo -e $PRIVATEKEY > ~/.ssh/id_rsa
+echo -e $EAWMVNKEY > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 eval "$(ssh-agent)"
 ssh-add ~/.ssh/id_rsa
